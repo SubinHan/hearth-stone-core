b86c6c0fd402d65321c8fe799cff072ddb4079db
수빈 한 <hangenius123@naver.com>
2023-01-04 16:28:24 +0900

    HearthstoneApi declaration modified.
    - converted string to string_view
    - every member function and variables are now public

--- HearthStoneFake/HearthstoneApi.cpp
+++ HearthStoneFake/HearthstoneApi.cpp
@@ -9,6 +9,7 @@
 #include <cstdlib>
 
 using namespace std;
+using namespace nyvux;
 
 const vector<string> HearthstoneApi::GetAllCardList()
 {
@@ -18,9 +19,13 @@ const vector<string> HearthstoneApi::GetAllCardList()
 
 	try
 	{
-		auto const Host = "us.api.blizzard.com";
+		//auto const Host = "us.api.blizzard.com";
 		auto const Port = "80";
-		auto const Target = "/hearthstone/cards";
+		//auto const Target = "/hearthstone/cards";
+
+		auto const Host = "oauth.battle.net";
+		auto const Target = "/token";
+
 		bool bIsVer10 = false;
 		int Version = bIsVer10 ? 10 : 11;
 
@@ -34,6 +39,14 @@ const vector<string> HearthstoneApi::GetAllCardList()
 		boost::beast::http::request<boost::beast::http::string_body> Req{ boost::beast::http::verb::get, Target, Version };
 		Req.set(boost::beast::http::field::host, UrlHost);
 		Req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+		const string Id = GetEnv(string{ HearthstoneApi::ENV_ID });
+		const string Secret = GetEnv(string{ HearthstoneApi::ENV_SECRET });
+		const string AuthContent = Id + ":" + Secret;
+		Req.set(boost::beast::http::field::authorization, AuthContent);
+
+		auto Body = Req.body();
+		Body.append("grant_type=client_credentials");
 
 		boost::beast::http::write(Stream, Req);
 
@@ -66,5 +79,8 @@ const vector<string> HearthstoneApi::GetAllCardList()
 
 const std::string HearthstoneApi::GetEnv(std::string Key)
 {
-	return getenv(Key.c_str());
+	char* env = nullptr;
+	_dupenv_s(&env, nullptr, Key.c_str());
+
+	return string(env);
 }
--- HearthStoneFake/HearthstoneApi.h
+++ HearthStoneFake/HearthstoneApi.h
@@ -3,8 +3,19 @@
 #include <vector>
 #include <string>
 
-class HearthstoneApi
+namespace nyvux
 {
-public:
-	static const std::vector<std::string> getAllCardList();	
-};
+	class HearthstoneApi
+	{
+	public:
+		static const std::vector<std::string> GetAllCardList();
+		static const std::string GetEnv(std::string Key);
+
+	public:
+		static constexpr std::string_view URL_HOST{ "oauth.battle.net" };
+		static constexpr std::string_view URL_TARGET_TOKEN{ "/token" };
+		static constexpr std::string_view ENV_ID{ "BLIZZARD_API_CLIENT_ID" };
+		static constexpr std::string_view ENV_SECRET{ "BLIZZARD_API_CLIENT_SECRET" };
+		
+	};
+}
